17051398940c8eef57af07082980f3600073f66b
Koji Wong <koji.wong@mail.utoronto.ca>
2023-12-10 00:40:36 -0500

    added examples and references

--- R/analysis.R
+++ R/analysis.R
@@ -1,16 +1,44 @@
-# ===== Package functions ======
-#' Preprocess 16S Data
+#' Preprocess 16S Sequence Data using Dada2 Package
 #'
 #' `preprocess_16s_data` preprocesses 16S rRNA single or paired-end reads
-#' that are in FASTQ format.
+#' that are in FASTQ format. This function calls the `filterAndTrim` function from
+#' the dada2 package which filters and trims fastq file inputs.
 #'
 #' @param input_dir directory containing desired input files. Must be in fastq.gz, fastq.bz2, or .fastq file
 #' @param output_dir target directory where preprocessed data will be saved. This is the same directory that will be used to learn error rates.
 #' @param verbose set to true for explicit updates during preprocessing
-#' @param multithread set to true if you wish to utilize multiple CPU cores
+#' @param multithread set to true if you wish to utilize multiple CPU cores for extra speed.
+#'
 #' @returns a dada output file
+#'
+#' #' @examples
+#' # Example:
+#' # Directory containing raw single-end 16S reads
+#' input <- file.path("sample_raw_16S_data", package = "CompHeatmaps")
+#' output <- file.path("/path/to/output_dir")
+#' \dontrun{
+#' library("CompHeatmaps")
+#' # Set verbose to TRUE for updates on progress in terminal and multithread to TRUE for faster computation if you have multiple cores.
+#' CompHeatmaps::preprocess_16s_data(input_dir = input, output_dir = output, verbose = TRUE, multithread = TRUE)
+#' length(result) # 4
+#' class(result) # list
+#' class(result[[1]]) # dada2
+#' }
+#'
 #' @import dada2
 #' @export
+#'
+#' @author {Koji Wong, \email{koji.wong@mail.utoronto.ca}}
+#'
+#' @references
+#' Structure, function and diversity of the healthy human microbiome.
+#' Human Microbiome Project Consortium, Nature, 486   (2012), pp. 207–214.
+#'
+#' Callahan BJ, McMurdie PJ, Rosen MJ, Han AW, Johnson AJA, Holmes SP (2016).
+#' “DADA2: High-resolution sample inference from Illumina amplicon data.”
+#' Nature Methods, 13, 581-583. doi:10.1038/nmeth.3869 \href{<https://doi.org/10.1038/nmeth.3869>}.
+#'
+#'
 preprocess_16s_data <- function(input_dir, output_dir, verbose = FALSE, multithread = FALSE) {
   # Check if directories are valid
   if (!is.character(input_dir) || length(input_dir) == 0 || !dir.exists(input_dir)) {
@@ -65,16 +93,37 @@ preprocess_16s_data <- function(input_dir, output_dir, verbose = FALSE, multithr
   if (verbose) {
     print("Preprocessing of 16S data done.")
   }
+  saveRDS(dada_out, "data/outputs/result")
   return(dada_out)
 }
 
-#' Creates an abundance matrix table
+#' Creates a matrix table of abundance counts of 16S reads across samples.
 #'
-#' `create_abundance_table` is used to create an abundance matrix table with dada2
+#' `create_abundance_table` is takes in as input a list of dada2 objects
+#' that have been filtered to create an abundance matrix table with dada2
 #' which will be used for heatmap generation. Performs some normalization
-#' and filtering to simplify results.
+#' and filtering to simplify results. Removes sequence variants identified as
+#' bimeric from the table to clean up further.
+#'
+#' @examples
+#' library("CompHeatmaps")
+#' /dontrun {
+#' # get filtered_reads from running `preprocess_16s_data()` on dataset
+#' table <- CompHeatmaps::create_abundance_table(filtered_reads)
+#' table[[1]] # -0.632696, abundance count after normalized and scaled
+#' dim(table) # 4 432, 4 rows each a sample in our input directory and 432 columns each a 16S sequence
+#' # can further explore data with `table[1, ]`, `table[, 1]`
+#' }
+#'
+#' @author {Koji Wong, \email{koji.wong@mail.utoronto.ca}}
+#'
+#' @references
+#' Callahan BJ, McMurdie PJ, Rosen MJ, Han AW, Johnson AJA, Holmes SP (2016).
+#' “DADA2: High-resolution sample inference from Illumina amplicon data.”
+#' Nature Methods, 13, 581-583. doi:10.1038/nmeth.3869 \href{<https://doi.org/10.1038/nmeth.3869>}.
 #'
 #' @param filtered_results the filtered results produced by dada2, a list of dada objects
+#' @param multithread set to TRUE to compute with multithreading for faster generation, only works with multiple cores
 #' @returns an abundance table
 #' @import dada2
 #' @export
@@ -111,14 +160,30 @@ create_abundance_table <- function(filtered_results, multithread = FALSE) {
 
 #' Generates a heatmap to illustrate the abundance of organisms across sample microbiomes.
 #'
-#' `create_heatmap` takes in an abundance table to generate a heatmap
-#' to illustrate abundance across samples.
+#' `create_heatmap` takes in an abundance matrix to generate a heatmap
+#' to illustrate abundance of 16S sequence variants across samples.
+#'
 #' @param table abundance table generated from `create_abundance_table`
 #' @param output_dir directory to save output heatmap object to
 #' @param proportion proportion of table between 0 and 1 to visualize. If 1, will visualize all columns, if 0.5 will visualize the first half of the columns of the table.
+#'
 #' @returns A heatmap object
+#'
+#' @examples
+#' \dontrun{
+#' plots_dir = "data/graphics
+#' create_heatmap(table, plots_dir, proportion = 0.025)
+#' }
+#'
+#' @author {Koji Wong, \email{koji.wong@mail.utoronto.ca}}
+#'
+#' @references
+#' Warnes G, Bolker B, Bonebakker L, Gentleman R, Huber W, Liaw A, Lumley T, Maechler M, Magnusson A,
+#' Moeller S, Schwartz M, Venables B (2022). gplots: Various R Programming Tools for Plotting Data. R
+#' package version 3.1.3, <https://CRAN.R-project.org/package=gplots>.
+#'
 #' @import gplots
-#' @imports ggplot2
+#' @import ggplot2
 #' @export
 create_heatmap <- function(table, output_dir, proportion = 1) {
   # Check table class type
@@ -153,6 +218,11 @@ create_heatmap <- function(table, output_dir, proportion = 1) {
               Colv = TRUE,
               Rowv = TRUE,
               col = heat.colors(112))
-  ggplot2::
-  saveRDS(heatmap, paste(output_dir, "heatmap", sep = "/"))
+  annotation = ggplot2::annotation_custom(grob = ggplot2::ggplotGrob(heatmap), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf)
+  heatmap_ggplot <- ggplot2::ggplot() + annotation + ggplot2::theme_void()
+  #saveRDS(heatmap, paste(output_dir, "heatmap", sep = "/"))
+  ggplot2::ggsave(file.path(output_dir, "heatmap.png"), plot = heatmap_ggplot, width = 6, height = 4)
+  return(heatmap)
 }
+# [END]
+
